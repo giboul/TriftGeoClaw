3a202ebb355a5c29037df5b1ecd4a45ac32b4910
giboul <axel.giboulot@epfl.ch>
2024-09-24 16:11:02 +0200

    custom interactive plots

--- iplot.py
+++ iplot.py
@@ -1,13 +1,28 @@
 from pathlib import Path
 from argparse import ArgumentParser
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-import numpy as np
 from clawpack.pyclaw.solution import Solution
 
 
-def plot_output(directory, fmt, animate):
+class Counter:
+    """Mutable object to keep track of current plot index."""
 
+    def __init__(self, ix: int = 0, max: int = np.inf):
+        self.ix = ix
+        self.max = max
+        self.keys = dict(right=1, left=-1, up=1, down=-1)
+
+    def increment(self, key):
+        """Get increment value (default is 0) and update counter."""
+        val = self.keys[key]
+        self.ix = (self.ix + val) % self.max
+
+
+def plot_output(directory, fmt, animate):
+    """Create new figure, plot initial solution and set up
+    interactive mode or animation."""
     # List output files
     outdir = Path(directory)
     frame_files = list(outdir.glob("fort.q*"))
@@ -24,11 +39,11 @@ def plot_output(directory, fmt, animate):
     ax.set_aspect("equal")
     fig.show()
 
-    # image details
-    xmin = solutions[0].states[0].patch.grid.c_centers[0][0, 0]
-    ymin = solutions[0].states[0].patch.grid.c_centers[1][0, 0]
-    xmax = solutions[0].states[0].patch.grid.c_centers[0][-1, -1]
-    ymax = solutions[0].states[0].patch.grid.c_centers[1][-1, -1]
+    # Get image extents
+    xmin = solutions[0].states[0].patch.grid.c_nodes[0][0, 0]
+    ymin = solutions[0].states[0].patch.grid.c_nodes[1][0, 0]
+    xmax = solutions[0].states[0].patch.grid.c_nodes[0][-1, -1]
+    ymax = solutions[0].states[0].patch.grid.c_nodes[1][-1, -1]
     q0 = solutions[0].states[0].q
     for state in solutions[0].states:
         X, Y = state.patch.grid.c_centers
@@ -43,17 +58,17 @@ def plot_output(directory, fmt, animate):
     im_extent = xmin, xmax, ymin, ymax
     mask = np.ma.MaskedArray(np.zeros((1,1)), mask=True)
 
-    # mutable incremented variable on arrow events
-    solix = dict(i=0)
-    def increment_solix(event):
-        if event.key == "right":
-            solix['i'] += 1
-        elif event.key == "left":
-            solix['i'] -= 1
-        solix['i'] = solix['i'] % nsolutions
-    fig.canvas.mpl_connect('key_press_event', increment_solix)
+    # Set up counter
+    counter = Counter(max=nsolutions)
+    def wait_update_key(event):
+        """If keys is in Couter.keys, increment index and update plot."""
+        if event.key in counter.keys:
+            counter.increment(event.key)
+            update(counter.ix)
+    fig.canvas.mpl_connect('key_press_event', wait_update_key)
 
     def update(i):
+        """Clear axis, show solution `i` and draw."""
         sol = solutions[i]
         ax.clear()
         ax.set_xlabel(r"$x$ [m]")
@@ -70,22 +85,18 @@ def plot_output(directory, fmt, animate):
             ax.imshow((eta-h).T, extent=patch_extent, interpolation="nearest",
                       origin="lower", cmap="inferno", vmin=zmin, vmax=zmax)
             ax.imshow(np.ma.MaskedArray(h, mask=h<=1e-3).T, extent=patch_extent,
-                      interpolation="nearest", origin="lower", cmap="Blues", vmin=0, vmax=hmax)
+                      interpolation="nearest", origin="lower",
+                      cmap="Blues", vmin=0, vmax=hmax)
         # Plot empty array in order to show the full extent
         # https://github.com/matplotlib/matplotlib/issues/19374
         ax.imshow(mask, extent=im_extent)
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-
-    def iplot():
-        while plt.fignum_exists(fig.number):
-            # Loop over images
-            update(solix['i'])
-            fig.waitforbuttonpress()
-
     if animate is False:
-        iplot()
+        update(0)
+        while plt.fignum_exists(fig.number):
+            fig.waitforbuttonpress()
     else:
         anim = FuncAnimation(fig, update, nsolutions, interval=200)
         anim.save("test.mp4")

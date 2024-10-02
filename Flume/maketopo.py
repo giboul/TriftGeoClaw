19d7dc2c325b308f64479b088d12d5c434f2e67b
giboul <axel.giboulot@epfl.ch>
2024-10-02 19:03:13 +0200

    renaming

---
+++ Flume/maketopo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+xlower = 0
+x_A    = 100.
+xupper = 400.
+
+slope = 0.05
+mx     = 1000 + 1
+z_A    = 0-slope*x_A
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    x = np.linspace(xlower, xupper, num=mx, endpoint=True)
+    z = - slope*x
+    z[x > x_A] = -slope*x_A
+
+    if False:
+        from matplotlib import pyplot as plt
+        plt.fill_between(x, z, z.min()-10)
+        plt.show()
+
+    np.savetxt(fname="topography.data",
+               X=np.vstack((x, z)).T,
+               header=f"{mx} # number of points",
+               comments="")

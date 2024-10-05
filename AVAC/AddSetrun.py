c0bf2f11fb7460fc27f50bb72733df21df11cc60
giboul <axel.giboulot@epfl.ch>
2024-10-05 10:10:43 +0200

    use addsetrun

--- AVAC/AddSetrun.py
+++ AVAC/AddSetrun.py
@@ -1,10 +1,14 @@
 #======= Topo =======
-sea_level = 1767
+lake_level = 1767
+dam_level = lake_level + 10
+flood_seed = 2670300, 1171500
 
 xmin = 2668500.
 xmax  = 2671000.
 ymin = 1170100.
 ymax  = 1171900.
+
+resolution = 10
 
 #======= Computation =======
 nx = 40
@@ -23,3 +27,5 @@ refinement_area = 0
 DryWetLimit = 0.0001
 
 nodatavalue = 1  # ??
+
+out_format = "binary"

e950ce2e1ebdb7deda7e778d0565e2bbc55afce1
giboul <axel.giboulot@epfl.ch>
2025-01-18 12:26:37 +0100

    observe longer tsunami

--- TSUL/setrun.py
+++ TSUL/setrun.py
@@ -88,7 +88,7 @@ def setrun(claw_pkg='geoclaw', bouss=False, avid='None', inflow="bc") -> ClawRun
     if clawdata.output_style==1:
         # Output nout frames at equally spaced times up to tfinal:
         clawdata.num_output_times = 50
-        clawdata.tfinal = 300
+        clawdata.tfinal = 600
         clawdata.output_t0 = True  # output at initial (or restart) time?
 
     elif clawdata.output_style == 2:

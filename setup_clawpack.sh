246b6599c84f50ab769f6a1a824a8b1b7691c72e
Axel Giboulot <74715790+giboul@users.noreply.github.com>
2025-06-07 04:02:10 -0700

    Update setup_clawpack.sh
    update for wsl compatibility

--- setup_clawpack.sh
+++ setup_clawpack.sh
@@ -1,10 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gfortran
+sudo apt-get install python3 python3-venv
+sudo apt-get install python3-dev
+sudo apt install liblapack-dev libopenblas-dev
 python3 -m venv claw
 export CLAW=$PWD/claw/src/clawpack
 echo "export CLAW=$CLAW" >> $PWD/claw/bin/activate
 source claw/bin/activate
 
+pip install --upgrade pip
 pip install meson-python
 pip install ninja
-pip install -r requirements.txt
+pip install pkgconfig
+pip install scipy
 pip install --no-build-isolation -e git+https://github.com/clawpack/clawpack.git@v5.11.0#egg=clawpack
-sudo apt install liblapack-dev libopenblas-dev
+pip install -r requirements.txt

f973f281df6d6af3c41b47830fe25de83296056d
giboul <axel.giboulot@epfl.ch>
2025-08-20 18:05:46 +0200

    add transparent cmap

--- pyvista_fgout_grid.py
+++ pyvista_fgout_grid.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 import numpy as np
 import pyvista as pv
-from utils import read_times, read_clawdata, read_fgout_bin
+import utils
 
 
 q0 = [0, 0, 0, 0]
@@ -15,26 +15,16 @@ def b(q, q0=q0): return q[3]-q[0]
 def z(q, q0=q0): return q[3]
 
 color_functions = dict(dh=dh, ds=ds, h=h, hu=hu, hv=hv, b=b, z=z)
-
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("outdir", type=str, nargs="?", default="_output")
-    parser.add_argument("--color_by", "-c", type=str, default="dh")
-    parser.add_argument("--gridno", "-g", type=int, default=1)
-    parser.add_argument("--cmaps", "-m", type=str, nargs=2, default=("gist_earth", "jet"))
-    parser.add_argument("--clim", "-l", type=float, nargs=2, default=(-1, 1))
-    parser.add_argument("--file_name", "-f", type=str, default="")
-    args = parser.parse_args()
-    return args
+utils.set_transparent_cmaps()
 
 def animation(outdir, color_by="dh", gridno=1, cmaps=("qist_earth", "jet"), clim=(-1, 1), file_name=""):
 
     outdir = Path(outdir)
     color_func = color_functions[color_by]
 
-    times = read_times(outdir, gridno)
+    times = utils.read_times(outdir, gridno)
 
-    data = read_clawdata(outdir/"fgout_grids.data", sep="#", skiprows=7)
+    data = utils.read_clawdata(outdir/"fgout_grids.data", sep="#", skiprows=7)
     x1, y1 = data["x1, y1"]
     x2, y2 = data["x2, y2"]
     nx, ny = data["nx,ny"]
@@ -42,7 +32,7 @@ def animation(outdir, color_by="dh", gridno=1, cmaps=("qist_earth", "jet"), clim
     y = np.linspace(y1, y2, ny)
     Y, X = np.meshgrid(y, x, copy=False)
 
-    q0 = read_fgout_bin(outdir, nx, ny, frameno=0)
+    q0 = utils.read_fgout_bin(outdir, nx, ny, frameno=0)
 
     p = pv.Plotter()
 
@@ -56,7 +46,7 @@ def animation(outdir, color_by="dh", gridno=1, cmaps=("qist_earth", "jet"), clim
 
     state = dict(i=0)
     def update(i):
-        q = read_fgout_bin(outdir, nx, ny, frameno=i)
+        q = utils.read_fgout_bin(outdir, nx, ny, frameno=i)
         bathy.points[:, 2] = b(q).T.flatten()
         surf.points[:, 2] = np.where(h(q)>0, z(q), np.nan).T.flatten()
         surf[color_by] = color_func(q, q0).T.flatten()
@@ -84,6 +74,17 @@ def animation(outdir, color_by="dh", gridno=1, cmaps=("qist_earth", "jet"), clim
     else:
         p.add_timer_event(max_steps=len(times), duration=500, callback=update)
         p.show()
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("outdir", type=str, nargs="?", default="_output")
+    parser.add_argument("--color_by", "-c", type=str, default="dh")
+    parser.add_argument("--gridno", "-g", type=int, default=1)
+    parser.add_argument("--cmaps", "-m", type=str, nargs=2, default=("gist_earth", "RdBu_water"))
+    parser.add_argument("--clim", "-l", type=float, nargs=2, default=(-0.5, 0.5))
+    parser.add_argument("--file_name", "-f", type=str, default="")
+    args = parser.parse_args()
+    return args
 
 def main():
     args = parse_args()

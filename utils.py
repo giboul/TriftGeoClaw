9a37fe6e40f2a50942f7f9e684a934b7a2be62aa
giboul <axel.giboulot@epfl.ch>
2025-08-20 18:04:33 +0200

    color defaults

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def read_geojson(path):
 
 water_color = (0.372, 0.588, 0.6)
 
-def set_transparent_cmaps(eps, water_color=water_color):
+def set_transparent_cmaps(eps=1e-1, water_color=water_color):
 
     mpl.colormaps.register(mpl.colors.LinearSegmentedColormap("Reds_water", {
         'red': ((0.0, water_color[0], water_color[0]),
@@ -82,18 +82,15 @@ def set_transparent_cmaps(eps, water_color=water_color):
 
 
     mpl.colormaps.register(mpl.colors.LinearSegmentedColormap("RdBu_water", {
-        'red': ((0.0, 1.0,1.0,),
-                (0.2, 1.0, 1.0),
+        'red': ((0.0, 0.0, 0.0,),
                 (0.5, water_color[0], water_color[0]),
-                (1.0, 0.0,0.0,),),
-        'green': ((0.0, 0.0,0.0,), 
-                (0.2, 0.0, 0.0),
+                (1.0, 1.0, 1.0,),),
+        'green': ((0.0, 0.0, 0.0,), 
                   (0.5, water_color[1], water_color[1]),
-                  (1.0, 0.0,0.0,),),
-        'blue': ((0.0, 0.0,0.0,),
-                (0.2, 0.0, 0.0),
+                  (1.0, 0.0, 0.0,),),
+        'blue': ((0.0, 1.0, 1.0,),
                  (0.5, water_color[2], water_color[2]),
-                 (1.0, 1.0,1.0,),),
+                 (1.0, 0.0, 0.0,),),
     }))
 
     mpl.colormaps.register(mpl.colors.LinearSegmentedColormap("RdBu_tc", {
@@ -107,9 +104,8 @@ def set_transparent_cmaps(eps, water_color=water_color):
                  (0.5, 1.0, 1.0),
                  (1.0, 1.0, 1.0),),
         'alpha': ((0.0, 1.0, 1.0),
-                  (0.5-eps, 1.0, 1.0),
-                  (0.5, 0.0, 0.0),
-                  (0.5+eps, 1.0, 1.0),
+                  (0.5-eps, 1.0, 0.0),
+                  (0.5+eps, 0.0, 1.0),
                   (1.0, 1.0, 1.0),),
     }))
 

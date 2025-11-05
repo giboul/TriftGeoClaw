7431b2e0ca52411c09d69581456a612f064f513c
giboul <axel.giboulot@epfl.ch>
2025-11-05 08:55:48 +0100

    Adding default values in config

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+from pathlib import Path
 from yaml import safe_load
 
-with open("config.yaml") as file:
+with open("_defaults.yaml") as file:
     config = safe_load(file)
+
+if Path("config.yaml").exists():
+
+    with open("config.yaml") as file:
+        custom = safe_load(file)
+
+    config.update(custom)
+
+if __name__ == "__main__":
+    print(config)
